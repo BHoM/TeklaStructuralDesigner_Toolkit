88797735a972a235e20f50ac2afb8d1f9ce07acc
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-10-07 10:38:09 +0100

    Toolkit solution created from Template

---
+++ TeklaStructuralDesigner_Engine/Convert/AdapterId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.TeklaStructuralDesigner
+{
+    public static partial class Convert
+    {
+        //Key used to find the software in the custom data of created or read objects
+        public const string AdapterId = "TeklaStructuralDesigner_id";
+    }
+}
